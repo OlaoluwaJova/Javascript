3eba34a54e98574c1d46e2d194a8dd2bcb9aca68
Olaoluwa Vincent <olaoluwavincent@gmail.com>
2022-03-25 11:08:10 +0100

    commit

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -1,2 +1,20 @@
-let cl = console.log(console);
-cl('Olaoluwa');
+let cl = console.log.bind(console);
+
+cl('jova');
+
+function ola() {
+    cl('Olaoluwa is the name of a King');
+}
+
+function vale() {
+    cl('Vale is the name of a King');
+}
+
+function repeatNTimes(f, num) {
+    for (let i = 0; i < num; i++) {
+        cl(num - 1);
+        cl(i);
+        f();
+    }
+}
+repeatNTimes(ola, 4);
